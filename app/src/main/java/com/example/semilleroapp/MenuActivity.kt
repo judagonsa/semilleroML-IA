e1d3458fdc6f0df5e1986fd0e000db95ec2c5bd6
jimmyale3102 <jimmyale3102@gmail.com>
2021-06-06 18:01:16 -0500

    Add listeners to menu buttons

--- app/src/main/java/com/example/semilleroapp/MenuActivity.kt
+++ app/src/main/java/com/example/semilleroapp/MenuActivity.kt
@@ -1,11 +1,23 @@
 package com.example.semilleroapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        initComponents()
+    }
+
+    private fun initComponents() {
+        video_to_text_button.setOnClickListener {
+            startActivity(Intent(this, VideoToTextActivity::class.java))
+        }
+        text_to_video_button.setOnClickListener {
+            startActivity(Intent(this, TextToVideoActivity::class.java))
+        }
     }
 }
