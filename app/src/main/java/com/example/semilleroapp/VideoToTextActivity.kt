1c3ad22d08d20515fc15d6b849efd0cba24e4326
jimmyale3102 <jimmyale3102@gmail.com>
2021-06-06 18:23:56 -0500

    Improve video to text activity

--- app/src/main/java/com/example/semilleroapp/VideoToTextActivity.kt
+++ app/src/main/java/com/example/semilleroapp/VideoToTextActivity.kt
@@ -1,12 +1,21 @@
 package com.example.semilleroapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_video_to_text.*
 
 class VideoToTextActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
         setContentView(R.layout.activity_video_to_text)
+        initComponents()
+    }
 
+    private  fun initComponents() {
+        close_button.setOnClickListener { finish() }
     }
 }
