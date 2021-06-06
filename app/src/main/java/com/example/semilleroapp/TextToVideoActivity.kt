8e25cda439b382b28156f37268726970a02ffd80
jimmyale3102 <jimmyale3102@gmail.com>
2021-06-06 17:59:32 -0500

    Create text to video activity

---
+++ app/src/main/java/com/example/semilleroapp/TextToVideoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.semilleroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TextToVideoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_to_video)
+    }
+}
