59cd550c73a112e25c32c0ccc1b89c8e6ceb4ba9
jimmyale3102 <jimmyale3102@gmail.com>
2021-06-07 17:21:02 -0500

    Add send icon to input message parent

--- app/src/main/java/com/example/semilleroapp/TextToVideoActivity.kt
+++ app/src/main/java/com/example/semilleroapp/TextToVideoActivity.kt
@@ -2,10 +2,12 @@ package com.example.semilleroapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_text_to_video.*
 
 class TextToVideoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_to_video)
+        input_message
     }
 }
