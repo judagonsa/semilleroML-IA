71c69063c41420a01cd28de3f483f669046e21e3
jimmyale3102 <jimmyale3102@gmail.com>
2021-06-07 17:48:12 -0500

    Create message item

---
+++ app/src/main/java/com/example/semilleroapp/adapter/MessageAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.semilleroapp.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.semilleroapp.R
+
+class MessageAdapter(
+    private val context: Context,
+    private val messages: ArrayList<String>
+): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return RecyclerView.ViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.message_item, parent, null)
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int = messages.size}
+
+
+}
