f90563f60df93b76ef461df28a007b312f377bff
judagonsa <judagonsa8@gmail.com>
2021-06-10 23:31:25 -0500

    Initial Commit

---
+++ AppSemillero/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppSemillero
+//
+//  Created by Julian González on 10/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
