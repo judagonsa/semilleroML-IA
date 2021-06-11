de84a2b4e3d2786a73de645fd1fdda7bedf9a2de
judagonsa <judagonsa8@gmail.com>
2021-06-11 00:41:06 -0500

    Update Home view primary

--- AppSemillero/AppDelegate.swift
+++ AppSemillero/AppDelegate.swift
@@ -7,11 +7,20 @@
 
 import UIKit
 
-@main
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let homeStoryBoard: UIStoryboard = UIStoryboard(name: "Home", bundle: nil)
+        let viewHome = homeStoryBoard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+        
+        viewHome.window = window
+        window?.rootViewController = viewHome
+        window?.makeKeyAndVisible()
         return true
     }
 
